d07df4013f50403742fe061016f3a4767762709d
iridion-github <antinoridavide@outlook.it>
2025-12-30 19:37:23 +0100

    Change base to lone slash

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/nos-qol/',
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
